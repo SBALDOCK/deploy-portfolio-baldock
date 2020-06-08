1dd3a2149753744c36a0c0223154bff8c1d07a88
SBALDOCK <stephenbaldock78@gmail.com>
2020-06-07 19:14:53 -0700

    assignment completion

--- public/assets/js/images.js
+++ public/assets/js/images.js
@@ -1,14 +1,39 @@
 "use strict"
 
-function randomImage(){
 
 let imagesArray = ['url("images/img_one.jpg")', 'url("images/img_two.jpg")', 'url("images/img_three.jpg")', 'url("images/img_four.jpg")', 'url("images/img_five.jpg")'];
 
-let randomNumber = Math.floor(Math.random() * imagesArray.length);
-
-document.getElementById('section.one').css = imagesArray[randomIndex];
-
-setTimeout(function() {
-  randomImage();
-}, 5000);
+let renderImages = () => {
+  setInterval(function(){
+    $('section.one').css(
+    {'background-image':imagesArray[1]});
+    }, 5000);
+  setInterval(function(){
+    $('section.one').css(
+    {'background-image':imagesArray[2]});
+    }, 10000);
+  setInterval(function(){
+    $('section.one').css(
+    {'background-image':imagesArray[3]});
+    }, 15000);
+  setInterval(function(){
+    $('section.one').css(
+    {'background-image':imagesArray[4]});
+    }, 20000);
+  setInterval(function(){
+    $('section.one').css(
+    {'background-image':imagesArray[0]});
+    }, 25000); 
 }
+
+renderImages();
+
+// $('section.one').css({'background-image':imagesArray[Math.floor(Math.random() * imagesArray.length)]});
+
+// // let randomNumber = Math.floor(Math.random() * imagesArray.length);
+
+// // document.getElementById('section.one').css= imagesArray[randomNumber];
+
+// setInterval(function() {
+//   randomImage();
+// }, 5000);
